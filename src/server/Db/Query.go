93c90455ebdbb5e61a3547daeaeba7fcb811de60
liu <back0893@163.com>
2020-04-01 02:48:15 +0800

    基础更新

--- src/server/Db/Query.go
+++ src/server/Db/Query.go
@@ -3,8 +3,10 @@ package Db
 import (
 	"database/sql"
 	"errors"
-	"golang.org/x/text/cases"
+	"fmt"
 	"reflect"
+	"strings"
+	"time"
 )
 
 /**
@@ -17,7 +19,65 @@ type Query struct {
 }
 
 func sKv(value reflect.Value) (keys, values []string) {
-
+	t := value.Type()
+	for i := 0; i < t.NumField(); i++ {
+		tf := t.Field(i)
+		vf := value.Field(i)
+		//不可导出
+		if tf.Anonymous {
+			continue
+		}
+		//非法零值
+		if !vf.IsValid() || reflect.DeepEqual(vf.Interface(), reflect.Zero(vf.Type()).Interface()) {
+			continue
+		}
+		//获得指针值
+		for vf.Type().Kind() == reflect.Ptr {
+			vf = vf.Elem()
+		}
+		//如果是组合后的struct,那么需要重复调用获得嵌套的struct值
+		if vf.Kind() == reflect.Struct && tf.Type.Name() != "Time" {
+			cKeys, cValues := sKv(vf)
+			keys = append(keys, cKeys...)
+			values = append(values, cValues...)
+		} else {
+			//依据json 获得tag的ksy忽略无tag的字段
+			key := strings.Split(tf.Tag.Get("json"), ",")[0]
+			if key == "" {
+				continue
+			}
+			value := format(vf)
+			if value != "" {
+				keys = append(keys, key)
+				values = append(values, value)
+			}
+		}
+	}
+	return
+}
+func format(value reflect.Value) string {
+	//格式化输出
+	if t, ok := value.Interface().(time.Time); ok {
+		return t.Format("2006-01-02 15:04:05")
+	}
+	switch value.Kind() {
+	case reflect.String:
+		return fmt.Sprintf(`'%s'`, value.Interface())
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return fmt.Sprintf(`%d`, value.Interface())
+	case reflect.Float32, reflect.Float64:
+		return fmt.Sprintf(`%f`, value.Interface())
+	case reflect.Slice, reflect.Array:
+		var values []string
+		for i := 0; i < value.Len(); i++ {
+			values = append(values, format(value.Index(i)))
+		}
+		return fmt.Sprintf(`(%s)`, strings.Join(values, ","))
+	case reflect.Interface:
+		return format(value.Elem())
+	default:
+		return ""
+	}
 }
 func mKv(value reflect.Value) (keys, values []string) {
 
